d836908d14fca78884f4c22ba4a48affd915b6e8
arnabdey24 <58462502+arnabdey24@users.noreply.github.com>
2020-10-04 09:51:47 +0600

    .

---
+++ uva_575.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+////////////////////////////////////////////////////////////////////////////<editor-fold desc="macros">
+#define endl "\n"
+#define long long long
+#define all(v) (v).begin(),(v).end()
+#define makeset(v) (v).resize(unique((v).begin(),(v).end())-(v).begin())
+#define IOS ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define filein freopen("input.txt", "r", stdin);
+#define fileout freopen("output.txt", "w", stdout);
+#define getline(x) getline(cin,x)
+#define makelower(s) transform(s.begin(),s.end(),s.begin(),::tolower)
+bool sortby(pair<int,int> a,pair<int,int> b){
+    double x=(double )a.first/a.second;
+    double y=(double )b.first/b.second;
+     return x<y;}
+long Pow(long x, long y) {
+    if(y == 0)
+        return 1;
+    if(y&1)
+        return x*Pow(x*x, y/2);
+    else
+        return Pow(x*x, y/2);
+}
+#define printcase(x)  cout<<"Case "<<in++<<": "<<x<<endl
+#define memset(a,b) memset(a,b,sizeof(a))
+#define print1d(ary) cout<<"[";for(auto x:(ary)){cout<<x<<",";}cout<<"]"<<endl;
+#define print2d(x) for(int m = 0; m <sizeof(x)/sizeof(x[0]) ; ++m){for(int l = 0; l <sizeof(x[0])/sizeof(x[0][0]) ; ++l){cout<<x[m][l]<<" ";}cout<<endl;}
+#define pi acos(-1)
+////////////////////////////////////////////////////////////////////////////</editor-fold>
+
+
+
+int main() {
+///<editor-fold desc="ios">
+    IOS
+#ifdef _AD_
+    filein //fileout
+#endif
+///</editor-fold>
+    long in=1,o;
+    long n;long k=0;
+    string s;
+    while (cin>>s && s!="0"){
+        //cout<<s<<endl;
+        long i=2;
+        long sum=0;
+        for(long j = s.length()-1; j >=0 ; --j){
+            long t=s[j]-'0';
+            sum+=t*(i-1);
+            i*=2;
+        }
+        cout<<sum<<endl;
+
+
+    }
+
+}
